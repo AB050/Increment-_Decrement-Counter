47d040a56d97822677f14c3386e51d37225a9f93
AB050 <abhinayaman94@gmail.com>
2023-05-03 15:25:48 +0530

    React Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((prevCount) => (prevCount += 1));
+  }
+  function decrement() {
+    setCount(function (prevCount) {
+      if (prevCount > 0) {
+        return (prevCount -= 1);
+      } else {
+        return (prevCount = 0);
+      }
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="h-screen flex flex-col justify-center items-center">
+        <h1 className="text-6xl font-bold mb-8">{count}</h1>
+        <div className="flex justify-center items-center">
+          <button
+            className="bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 rounded-md mr-3"
+            onClick={increment}
+          >
+            Increment
+          </button>
+          <button
+            className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-md"
+            onClick={decrement}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
